530f7eadf21796d4a1b2f9dfb6bc245f31e0a6f6
kassidyb816 <kassidy.barnes65@gmail.com>
2021-02-28 16:21:38 -0600

    Auth)

--- server.js
+++ server.js
@@ -143,7 +143,26 @@ app.delete('/ticket/:id', (request, response) => {
             });
         });
     });
+  
+    const { auth } = require('express-openid-connect');
+
+    const config = {
+      authRequired: false,
+      auth0Logout: true,
+      secret: 'a long, randomly-generated string stored in env',
+      baseURL: 'http://localhost:8080',
+      clientID: '30honhSHwbI0rGGsbmfNLbBSx2fMggre',
+      issuerBaseURL: 'https://dev-1qgt6cpj.us.auth0.com'
+    };
     
+    // auth router attaches /login, /logout, and /callback routes to the baseURL
+    app.use(auth(config));
+    
+    // req.isAuthenticated is provided from the auth router
+    app.get('/', (req, res) => {
+      res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    });
+        
 
 // start server
 // last method to execute
